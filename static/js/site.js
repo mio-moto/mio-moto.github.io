65b984fe5b81da62584d857df761c52e26c1a0f2
Martin Zier <DarkMio@users.noreply.github.com>
2024-08-06 01:10:38 +0200

    feat: reflecting name change

--- static/js/site.js
+++ static/js/site.js
@@ -1,88 +1,152 @@
-window.addEventListener('DOMContentLoaded', () => {
-    'use strict';
-    
-    [...document.getElementsByTagName("nav")].forEach(x => registerNavigation(x, ["section", "fragment"]));
+window.addEventListener("DOMContentLoaded", () => {
+  "use strict";
 
-    document.getElementById("theme-toggle").addEventListener("click", (e) => {
-        if(e.target.checked) {
-            document.body.classList.remove("dark");
-        } else {
-            document.body.classList.add("dark");
-        }
-    })
+  [...document.getElementsByTagName("nav")].forEach((x) =>
+    registerNavigation(x, ["section", "fragment"])
+  );
+
+  document.getElementById("theme-toggle").addEventListener("click", (e) => {
+    if (e.target.checked) {
+      document.body.classList.remove("dark");
+    } else {
+      document.body.classList.add("dark");
+    }
   });
 
-  /**
-   * 
-   * @param {HTMLElement} navElement 
-   */
-  const traverseTree = (navElement) => {
-    const ids = [];
-    for(let element = navElement.parentNode; element !== null; element = element.parentNode) {
-        if(element.id) {
-            ids.push(element.id);
-        }
-    }
-    return ids.reverse();
+  {
+    const writeSpace = document.getElementById("name");
+    writeSpace.innerText = "Martin Zier";
+    const text = "\r\r\r\r\r\r\r\r\r\r\rMio Moto";
+    let lastBlinker;
+    let i = 0;
+    let abort = false;
+    const write = () => {
+      if (i >= text.length) {
+        abort = true;
+        return;
+      }
+      const letter = text[i++];
+      if (letter == "\r") {
+        writeSpace.textContent = writeSpace.textContent.slice(0, -1);
+        setTimeout(write, 50);
+      } else {
+        writeSpace.textContent = writeSpace.textContent + letter;
+        setTimeout(write, Math.random() * 120);
+      }
+    };
+    let step = 0;
+    const blink = () => {
+      if (step == 10) {
+        write();
+        return;
+      }
+      if (step % 2 == 0) {
+        writeSpace.textContent = writeSpace.textContent + "_";
+      } else {
+        writeSpace.textContent = writeSpace.textContent.slice(0, -1);
+      }
+      step++;
+      setTimeout(blink, 500);
+    };
+    const blinky = () => {
+      step %= 2;
+      step++;
+      if (step % 2 == 0) {
+        lastBlinker.textContent = "";
+      } else {
+        lastBlinker.textContent = "_";
+      }
+      if (!aborted) {
+        lastBlinker.textContent = "";
+        setTimeout(blinky, 500);
+      }
+    };
+    blink();
   }
+});
 
-  /**
-   * @param {HTMLElement} element 
-   */
-  const hashHref = (element) => element.getAttribute("href").replace("#", "");
-
-  /**
-   * Hook up events under the assumption that there's 
-   * @param {HTMLElement} navElement required element that's the navigation of layout <nav><a/><a/>...</nav>
-   * @param {string[]} childTagNames required tag names that are adjacent to the nav <parent><nav/><tagName/><tagName/>...</parent>
-   */
-  const registerNavigation = (navElement, childTagNames) => {
-    const parentNavigation = traverseTree(navElement);
-    const parentFragment = parentNavigation.length <= 0 ? "" : `/${parentNavigation.join("/")}`;
-    const targetElements = ([...navElement.parentElement.childNodes.values()]).filter(x => childTagNames.includes(x.tagName?.toLowerCase()));
-    const linkTargets = [...navElement.childNodes.values()].filter(x => (x.tagName?.toLowerCase() === "a"));
-    const hrefs = linkTargets.map(x => hashHref(x));
-
-    // primary method to switch child views based on top nav
-    const setNavElementActive = (activeNav, enableUri) => {
-        // disable all elements, reset
-        targetElements.forEach(x => x.style.display = "none");
-        linkTargets.forEach(x => x.classList.remove("selected"));
-        // get the id and its associated active view element
-        const id = hashHref(activeNav);
-        if(enableUri) {
-            const uri = `#${parentFragment}/${id}`;
-            document.location.hash = uri;
-        }
-        const activeView = targetElements.find(x => x.id === id);
-        if(!activeView) {
-            console.error(`no nav element fitting the name of ${id}`);
-            return;
-        }
-        // set them visible and active
-        activeView.style.display = 'inherit';
-        activeNav.classList.add("selected");
+/**
+ *
+ * @param {HTMLElement} navElement
+ */
+const traverseTree = (navElement) => {
+  const ids = [];
+  for (
+    let element = navElement.parentNode;
+    element !== null;
+    element = element.parentNode
+  ) {
+    if (element.id) {
+      ids.push(element.id);
     }
+  }
+  return ids.reverse();
+};
 
-    const activateDocumentLocation = () => {
-        const location = document.location.hash.replace("#", "");
-        const components = location.split("/").filter(x => x.length > 0);
-        const targetViewId = components.slice(0, parentNavigation.length + 1).pop();
-        const hrefIndex = hrefs.indexOf(targetViewId);
-        if(hrefIndex < 0) {
-            setNavElementActive(linkTargets[0], false);
-            return;
-        }
-        setNavElementActive(linkTargets[hrefIndex], false);
+/**
+ * @param {HTMLElement} element
+ */
+const hashHref = (element) => element.getAttribute("href").replace("#", "");
+
+/**
+ * Hook up events under the assumption that there's
+ * @param {HTMLElement} navElement required element that's the navigation of layout <nav><a/><a/>...</nav>
+ * @param {string[]} childTagNames required tag names that are adjacent to the nav <parent><nav/><tagName/><tagName/>...</parent>
+ */
+const registerNavigation = (navElement, childTagNames) => {
+  const parentNavigation = traverseTree(navElement);
+  const parentFragment =
+    parentNavigation.length <= 0 ? "" : `/${parentNavigation.join("/")}`;
+  const targetElements = [
+    ...navElement.parentElement.childNodes.values(),
+  ].filter((x) => childTagNames.includes(x.tagName?.toLowerCase()));
+  const linkTargets = [...navElement.childNodes.values()].filter(
+    (x) => x.tagName?.toLowerCase() === "a"
+  );
+  const hrefs = linkTargets.map((x) => hashHref(x));
+
+  // primary method to switch child views based on top nav
+  const setNavElementActive = (activeNav, enableUri) => {
+    // disable all elements, reset
+    targetElements.forEach((x) => (x.style.display = "none"));
+    linkTargets.forEach((x) => x.classList.remove("selected"));
+    // get the id and its associated active view element
+    const id = hashHref(activeNav);
+    if (enableUri) {
+      const uri = `#${parentFragment}/${id}`;
+      document.location.hash = uri;
     }
-    activateDocumentLocation();
+    const activeView = targetElements.find((x) => x.id === id);
+    if (!activeView) {
+      console.error(`no nav element fitting the name of ${id}`);
+      return;
+    }
+    // set them visible and active
+    activeView.style.display = "inherit";
+    activeNav.classList.add("selected");
+  };
 
-    // set the first nav element as active (as a reasonable guess)
-    // setNavElementActive(linkTargets[0]);
-    // hook up the on-click event - prevent scrolling and history pushing
-    linkTargets.forEach(x => x.addEventListener("click", (e) => {
-        e.preventDefault();
-        setNavElementActive(x, true);
-        return false;
-    }))
-}
+  const activateDocumentLocation = () => {
+    const location = document.location.hash.replace("#", "");
+    const components = location.split("/").filter((x) => x.length > 0);
+    const targetViewId = components.slice(0, parentNavigation.length + 1).pop();
+    const hrefIndex = hrefs.indexOf(targetViewId);
+    if (hrefIndex < 0) {
+      setNavElementActive(linkTargets[0], false);
+      return;
+    }
+    setNavElementActive(linkTargets[hrefIndex], false);
+  };
+  activateDocumentLocation();
+
+  // set the first nav element as active (as a reasonable guess)
+  // setNavElementActive(linkTargets[0]);
+  // hook up the on-click event - prevent scrolling and history pushing
+  linkTargets.forEach((x) =>
+    x.addEventListener("click", (e) => {
+      e.preventDefault();
+      setNavElementActive(x, true);
+      return false;
+    })
+  );
+};
